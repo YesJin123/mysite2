1a970ad24f27958e350b9a33a8f245f41d3d2250
Yesjin123 <dnwls2514@naver.com>
2025-05-08 20:22:55 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import HomePage from './pages/home/HomePage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 // import GalleryPage from './pages/gallery/GalleryPage';
@@ -11,14 +11,15 @@ import Footer from './components/common/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='mysite2'>
       <div className="App">
         <header className="app-header">
           <Header />
         </header>
         <main>
           <Routes>
-            <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<HomePage />} />
             <Route path="/about-me" element={<AboutMePage />} />
             {/*<Route path="/gallery" element={<GalleryPage />} />
             <Route path="/blog" element={<BlogPage />} />
